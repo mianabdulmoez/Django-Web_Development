e250668572c839f2b579860106b64f0baca351e2
Abdul Moeez <91822215+mianabdulmoez@users.noreply.github.com>
2024-06-15 02:39:46 +0500

    commit

--- django_web/calc/views.py
+++ django_web/calc/views.py
@@ -6,16 +6,17 @@ def home(request):
     return render(request, 'home.html', {'name': 'Abdul Moeez'})
     # return render(request, 'home.html', {'name',input("Enter your name")})
     
+# Fetching Data
+# def add(request):
+#     val1 = int(request.GET['num1'])
+#     val2 = int(request.GET['num2'])
+#     res = int(val1) + int(val2)
+#     return render(request, 'result.html', {'result': res})
 
-def add(request):
+# Submitting Data
+def add(request, methods=['POST']):
     val1 = int(request.GET['num1'])
     val2 = int(request.GET['num2'])
     res = int(val1) + int(val2)
     return render(request, 'result.html', {'result': res})
-    
-# def add(request):
-#     val1 = int(request.POST["num1"])
-#     val2 = int(request.POST["num2"])
-#     res = val1 + val2
-#     return render(request, 'result.html', {'result': res})
     # return render(request, 'result.html', {'result': res})
